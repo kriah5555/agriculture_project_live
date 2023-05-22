ce4d60e228f2177d01d32ca9dce21bdfa52a325e
sumanthsumu1418 <sumanth1418@gmail.com>
2023-05-22 14:36:09 +0530

    code update

--- agriculture_project_backend/settings.py
+++ agriculture_project_backend/settings.py
@@ -81,7 +81,7 @@ WSGI_APPLICATION = 'agriculture_project_backend.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': str(BASE_DIR / "db.sqlite3"),
     }
 }
 
