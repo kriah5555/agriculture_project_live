7340d4ce21066d8812c49237b5b0099a7f79d935
sumanthsumu1418 <sumanth1418@gmail.com>
2023-05-31 14:57:49 +0530

    DB creds updated

--- agriculture_project_backend/settings.py
+++ agriculture_project_backend/settings.py
@@ -80,8 +80,12 @@ WSGI_APPLICATION = 'agriculture_project_backend.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'myproject',
+                'USER': 'myprojectuser',
+                'PASSWORD': 'password',
+                'HOST': 'localhost',
+                'PORT': '',
     }
 }
 
@@ -137,7 +141,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-STATICFILES_DIRS =[
+STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static')
 ]
 
