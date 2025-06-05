e5156d8a668bfee3270301be3aa8e035a33c298c
Yashwanth A <yash2706.git@gmail.com>
2025-06-05 15:19:48 +0530

    Final backup before merge

--- agriculture_project_backend/settings.py
+++ agriculture_project_backend/settings.py
@@ -94,7 +94,11 @@ DATABASES = {
          'ENGINE': 'django.db.backends.sqlite3',
          'NAME': BASE_DIR / 'db.sqlite3',
      }
+<<<<<<< Updated upstream
 }
+=======
+ }
+>>>>>>> Stashed changes
 
 # fro authentication
 # REST_FRAMEWORK = {
