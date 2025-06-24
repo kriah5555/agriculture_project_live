87aaadc432272700d26282c82a3fb5a4e74af270
Yashwanth A <yash2706.git@gmail.com>
2025-06-24 18:03:06 +0530

    Added timings in Admin dashboard, Added Recommendation section in the API details, Added Recommendation dashboard

--- predicter/urls.py
+++ predicter/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('api/predict/<int:api_id>/', views.get_recommendation, name='get_recommendation'),
+    #path('api/predict/<int:api_id>/', views.get_recommendation, name='get_recommendation'),
+    path('crop-recommendation-dashboard/', views.get_recommendation, name='dashboard'),
 ]
--- predicter/views.py
+++ predicter/views.py
@@ -1,12 +1,19 @@
 #from django.shortcuts import render
 
 # Create your views here.
-from django.http import JsonResponse
+from django.http import HttpResponseBadRequest
 from . ai_model.model import run_model
 from agriapp.models import DeviseApis
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import get_object_or_404, render
 
-def get_recommendation(request, api_id):
+def get_recommendation(request):
+    api_id = request.GET.get("api_id")
+    if not api_id:
+        return HttpResponseBadRequest("Missing api_id parameter.")
+    try:
+        api_id = int(api_id)
+    except ValueError:
+        return HttpResponseBadRequest("Invalid 'api_id' parameter.")
 ### Test:
 # ------------- Comment or Remove below during deployment -------------
     #59,55,79,20.36720401,16.89574311,8.766128654,82.2545577,chickpea
@@ -15,10 +22,10 @@ def get_recommendation(request, api_id):
     #118,21,34,24.38534644,64.72543073,7.234258375,119.6324109,coffee
     soil_nutrients = {'N': [118], 'P': [21], 'K': [34], 'temperature': [24.34986394], 'humidity':[64.79187265], 'ph': [7.287268394], 'rainfall': [119.63636055]}        
     result = run_model(soil_nutrients)
-    return render(request, 'predicted_popup.html', {'recommendation': result,'soil_nutrients':soil_nutrients})
+    return render(request, 'crop_recom_dashb.html', {'recommendation': result, 'soil_nutrients': soil_nutrients})
 # ---------------------------------------------------------------------
-    if request.method == "POST":
-        data_instance = get_object_or_404(DeviseApis, id=api_id)
+#    if request.method == "POST":
+#        data_instance = get_object_or_404(DeviseApis, id=api_id)
 #---------- Remove below comment during deployment -------------
 #            soil_nutrients = {
 #                'N':[float(instance.nitrogen)],
