3c95d066383d6a7d3d751f17fecae4a6bfa14e06
Yashwanth A <yash2706.git@gmail.com>
2025-07-05 09:21:23 +0530

    Added Pie chart for each soil nutrient, Added sustainability indicater for each soil nutrient in Additional recommendations

--- predicter/models.py
+++ predicter/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+
+
+# Not a django database model, no migrations needed
+class Crop:
+    def __init__(self, name, image_name):
+        self.name = name
+        self.image_name = image_name
